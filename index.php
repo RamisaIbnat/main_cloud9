fb8a63ca616dad55b84bae8ccdfece3aa9078771
ramisa-ib <ramisa.ib1@gmail.com>
2018-11-29 03:14:10 +0600

    login

--- index.php
+++ index.php
@@ -24,8 +24,6 @@
   <script src='https://code.jquery.com/jquery-1.12.0.min.js'></script>
   <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="crossorigin="anonymous"></script>
   <script src="js/jquery.fullpage.js"></script>
-
-
 </head>
 <body>
 <!-- Slideshow container -->
--- login.php
+++ login.php
@@ -21,9 +21,30 @@
  ?>
 <html>
 	<head>
-		<title>blah</title>
+		<title>login</title>
+		<link rel="Stylesheet" href="css\style.css">
+		<link href="https://fonts.googleapis.com/css?family=Cinzel+Decorative:700|Cinzel:700|Oswald" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css?family=Crimson+Text|Fira+Sans|PT+Sans+Narrow|Roboto+Condensed" rel="stylesheet">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href="https://fonts.googleapis.com/css?family=Pragati+Narrow" rel="stylesheet">
+		<meta charset=="utf-8">
+		<script src='https://code.jquery.com/jquery-1.12.0.min.js'></script>
+		<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="crossorigin="anonymous"></script>
+		<script src="js/jquery.fullpage.js"></script>
 	</head>
 	<body>
+	<div id='fullpage'>
+		<div class="slideshow-container">
+			<div class="mySlides fade" id="image1">
+		    	<img class='SlideImage'  src="css/images/telecom6.jpg">
+		    </div>
+		    <div class="mySlides fade" id="image2">
+		    	<img class='SlideImage'  src="css/images/telecom21.jpg">
+		  	</div>
+		  	<div class="mySlides fade" id="image3">
+		    	<img class='SlideImage'  src="css/images/telecom4.jpg">
+		    </div>
+		</div>
 	<h1>Login</h1>
 	<form action="" method="POST">
 		username: <input type="text" name="uname"/><br>
@@ -31,5 +52,6 @@
 		<input type="submit" name="submit"/>
 	</form>
 	or <a href="signup.php">Signup</a>
+	<script src="js/slideshow.js"></script>
 	</body>
 </html>
